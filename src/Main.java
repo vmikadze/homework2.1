6e892121aa58113168bae9dc3e40cb9db6bcbb7f
Vladimir Mikadze <v.mikadze10@gmail.com>
2023-02-08 20:33:22 +0400

    task done

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10000;
+        int bonus = 20;
+
+        int x = price / bonus;
+
+        System.out.println(x + " миль начисленных за купленный билет");
+    }
+}
